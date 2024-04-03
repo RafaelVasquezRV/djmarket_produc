653c2351af90ad8d9c62d7d88d11ff53ef1f1e4f
Rafael Vásquez <94866887+RafaelVasquezRV@users.noreply.github.com>
2024-04-02 20:09:34 -0400

    Update3 local.py

--- setting_djmarket/settings/local.py
+++ setting_djmarket/settings/local.py
@@ -4,7 +4,7 @@ from .db import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['ravasquez.pythonanywhere.com','localhost', '127.0.0.1']
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
